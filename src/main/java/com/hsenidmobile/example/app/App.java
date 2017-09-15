d7de35fa49cf69fac0169236466e316be0c3eeec
ThisaraHMS <thisarap@hsenidmobile.com>
2017-09-15 11:34:47 +0530

    maven example

---
+++ src/main/java/com/hsenidmobile/example/app/App.java
@@ -0,0 +1,13 @@
+package com.hsenidmobile.example.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/hsenidmobile/example/app/TestClass.java
@@ -0,0 +1,15 @@
+package com.hsenidmobile.example.app;/**
+ * Created by hsenid on 9/15/17.
+ */
+
+import org.testng.annotations.Test;
+
+/**
+ * thisara
+ * hsenid mobile
+ * training
+ */
+@Test ()
+public class TestClass {
+
+}
